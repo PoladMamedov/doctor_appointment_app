ed59e77c5125c31b68fb92f3e7ea696c450e01dd
Egoor_03 <e.zavertalyuk@gmail.com>
2023-04-05 20:50:20 +0300

    test

--- js/Modal.js
+++ js/Modal.js
@@ -1,19 +1,19 @@
-export default class Modal{
-    render(){
+export default class Modal {
+    render() {
         const form = document.createElement('form');
         form.classList.add(
-        'w-50',
-        'd-flex',
-        'flex-column',
-        'border',
-        'border-primary',
-        'rounded',
-        'p-4',
-        "position-absolute",
-        "top-50",
-        "start-50",
-        "translate-middle",
-        "bg-light"
+            'w-50',
+            'd-flex',
+            'flex-column',
+            'border',
+            'border-primary',
+            'rounded',
+            'p-4',
+            "position-absolute",
+            "top-50",
+            "start-50",
+            "translate-middle",
+            "bg-light"
         );
         form.innerHTML = `<div class="mb-3">
         <label class="form-label">Email address</label>
@@ -26,16 +26,20 @@ export default class Modal{
         const btnForm = document.createElement('button');
         btnForm.type = 'submit';
         btnForm.innerText = 'Увійти';
-        btnForm.classList.add('btn','btn-primary');  
+        btnForm.classList.add('btn', 'btn-primary');
         const btnCancel = document.createElement('button');
         btnCancel.type = 'reset';
         btnCancel.innerHTML = 'Скасувати'
         btnCancel.classList.add('btn', 'btn-danger', 'mt-2')
         form.append(btnForm, btnCancel);
-        btnCancel.addEventListener('click', ()=>{
+        btnCancel.addEventListener('click', () => {
             form.remove()
-        })       
-        return form;  
+        })
+
+        return form;
     };
 }
+
+
+
 
