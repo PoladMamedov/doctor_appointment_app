3cbc5594226fa229383761a6d2d6039e0a08eea4
Egoor_03 <e.zavertalyuk@gmail.com>
2023-04-05 22:02:55 +0300

    rework modal

--- js/Modal.js
+++ js/Modal.js
@@ -43,3 +43,65 @@ export default class Modal {
 
 
 
+// export default class Modal {
+//     constructor() {
+//         this.form = null;
+//     }
+
+//     render() {
+//         this.form = document.createElement('form');
+//         this.form.classList.add(
+//             'w-50',
+//             'd-flex',
+//             'flex-column',
+//             'border',
+//             'border-primary',
+//             'rounded',
+//             'p-4',
+//             "position-absolute",
+//             "top-50",
+//             "start-50",
+//             "translate-middle",
+//             "bg-light"
+//         );
+//         this.form.innerHTML = `<div class="mb-3">
+//         <label class="form-label">Email address</label>
+//         <input type="email" class="form-control" required>
+//         </div>
+//         <div class="mb-3">
+//         <label class="form-label">Password</label>
+//         <input type="password" class="form-control" required>
+//         </div>`
+//         const btnForm = document.createElement('button');
+//         btnForm.type = 'submit';
+//         btnForm.innerText = 'Увійти';
+//         btnForm.classList.add('btn', 'btn-primary');
+//         const btnCancel = document.createElement('button');
+//         btnCancel.type = 'reset';
+//         btnCancel.innerHTML = 'Скасувати'
+//         btnCancel.classList.add('btn', 'btn-danger', 'mt-2')
+//         btnCancel.addEventListener('click', () => {
+//             this.deleteForm();
+//         });
+//         this.form.append(btnForm, btnCancel);
+//         document.body.prepend(this.form);
+//         return this.form;
+//     }
+
+//     deleteForm() {
+//         this.form.remove();
+//     }
+// }
+
+// const modal = new Modal();
+// const authorizationBtn = document.querySelector('.authorization-btn');
+// authorizationBtn.addEventListener('click', () => {
+//     modal.render();
+// });
+
+// document.addEventListener('click', (event) => {
+//     if (event.target !== modal.form && !modal.form.contains(event.target)) {
+//         modal.deleteForm();
+//     }
+// });
+
