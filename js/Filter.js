91431293ecbd6eec21413b53b6d7bdc2d99f7dc5
Egoor_03 <e.zavertalyuk@gmail.com>
2023-04-06 11:08:54 +0300

    add filter to classes

--- js/Filter.js
+++ js/Filter.js
@@ -1,55 +1,63 @@
 
+class VisitFilters {
+   constructor(list, visitCards) {
+      this.list = list;
+      this.visitCards = visitCards;
+      this.filters = {
+         searchText: '',
+         status: '',
+         urgency: '',
+      };
+   }
+
+   applyFilters() {
+      this.visitCards.forEach((card) => {
+         const cardStatus = card.querySelector('.visit-status').textContent.trim();
+         const cardUrgency = card.querySelector('.visit-urgency').textContent.trim();
+         const cardName = card.querySelector('p').textContent.toLowerCase().replace(/\s/g, '');
+         const isNameMatch = cardName.includes(this.filters.searchText);
+
+         const isStatusMatch =
+            this.filters.status === '' ||
+            this.filters.status === 'Статус визита' ||
+            this.filters.status === cardStatus;
+
+         const isUrgencyMatch =
+            this.filters.urgency === '' ||
+            this.filters.urgency === 'Срочность визита' ||
+            this.filters.urgency === cardUrgency;
+
+         if (isNameMatch && isStatusMatch && isUrgencyMatch) {
+            card.style.display = 'block';
+         } else {
+            card.style.display = 'none';
+         }
+      });
+   }
+}
+
 const searchInput = document.getElementById('searchInput');
 const list = document.querySelector('.visit-wrap');
 const selectStatus = document.querySelector('.select-options');
 const selectUrgency = document.querySelector('.select-urgency');
 const visitCards = document.querySelectorAll('.visit-wrap .visit-card');
-const moreBtn = document.querySelectorAll('.more-btn ');
-const statusWrap = document.querySelectorAll('.status-wrap ');
-const defaultStatus = 'Статус визита';
-const defaultUrgency = 'Срочность визита';
 
-const filters = {
-   searchText: '',
-   status: defaultStatus,
-   urgency: defaultUrgency
-};
+const filters = new VisitFilters(list, visitCards);
 
-searchInput.addEventListener('input', function () {
-   filters.searchText = searchInput.value.toLowerCase().replace(/\s/g, '');
-   applyFilters();
+searchInput.addEventListener('input', () => {
+   filters.filters.searchText = searchInput.value.toLowerCase().replace(/\s/g, '');
+   filters.applyFilters();
 });
 
-selectStatus.addEventListener('change', function () {
-   filters.status = selectStatus.value;
-   applyFilters();
+selectStatus.addEventListener('change', () => {
+   filters.filters.status = selectStatus.value;
+   filters.applyFilters();
 });
 
-selectUrgency.addEventListener('change', function () {
-   filters.urgency = selectUrgency.value;
-   applyFilters();
+selectUrgency.addEventListener('change', () => {
+   filters.filters.urgency = selectUrgency.value;
+   filters.applyFilters();
 });
-
-function applyFilters() {
-   visitCards.forEach(function (card) {
-      const cardStatus = card.querySelector('.visit-status').textContent.trim();
-      const cardUrgency = card.querySelector('.visit-urgency').textContent.trim();
-      const cardName = card.querySelector('p').textContent.toLowerCase().replace(/\s/g, '');
-
-      const isNameMatch = cardName.includes(filters.searchText);
-
-      const isStatusMatch = filters.status === defaultStatus || filters.status === '' || filters.status === cardStatus;
-
-
-      const isUrgencyMatch = filters.urgency === defaultUrgency || filters.urgency === '' || filters.urgency === cardUrgency;
-
-      if (isNameMatch && isStatusMatch && isUrgencyMatch) {
-         card.style.display = 'block';
-      } else {
-         card.style.display = 'none';
-      }
-   });
-}
 
 
 
--- js/script.js
+++ js/script.js
@@ -3,74 +3,72 @@ import Modal from "./Modal.js";
 import DoctorAPIService from "./doctor_api_service.js";
 import VisitForm from "./visit.js";
 
-// чтобы работала форма визита нужно раскоментировать кнопку и код ниже, код ниже будет запускаться после входа и появления кнопки создать визит.
-
 const form = new VisitForm();
 const createVisitBtn = document.querySelector("#create-visit-btn");
 createVisitBtn.addEventListener("click", (e) => {
   const visitForm = form.render();
   document.body.prepend(visitForm);
 });
+class VisitFilters {
+  constructor(list, visitCards) {
+    this.list = list;
+    this.visitCards = visitCards;
+    this.filters = {
+      searchText: '',
+      status: '',
+      urgency: '',
+    };
+  }
 
-const searchInput = document.getElementById("searchInput");
-const list = document.querySelector(".visit-wrap");
-const selectStatus = document.querySelector(".select-options");
-const selectUrgency = document.querySelector(".select-urgency");
-const visitCards = document.querySelectorAll(".visit-wrap .visit-card");
-const moreBtn = document.querySelectorAll(".more-btn ");
-const statusWrap = document.querySelectorAll(".status-wrap ");
-const defaultStatus = "Статус визита";
-const defaultUrgency = "Срочность визита";
+  applyFilters() {
+    this.visitCards.forEach((card) => {
+      const cardStatus = card.querySelector('.visit-status').textContent.trim();
+      const cardUrgency = card.querySelector('.visit-urgency').textContent.trim();
+      const cardName = card.querySelector('p').textContent.toLowerCase().replace(/\s/g, '');
+      const isNameMatch = cardName.includes(this.filters.searchText);
 
-const filters = {
-  searchText: "",
-  status: defaultStatus,
-  urgency: defaultUrgency,
-};
+      const isStatusMatch =
+        this.filters.status === '' ||
+        this.filters.status === 'Статус визита' || // добавлено условие для показа всех карточек
+        this.filters.status === cardStatus;
 
-searchInput.addEventListener("input", function () {
-  filters.searchText = searchInput.value.toLowerCase().replace(/\s/g, "");
-  applyFilters();
-});
+      const isUrgencyMatch =
+        this.filters.urgency === '' ||
+        this.filters.urgency === 'Срочность визита' ||
+        this.filters.urgency === cardUrgency;
 
-selectStatus.addEventListener("change", function () {
-  filters.status = selectStatus.value;
-  applyFilters();
-});
-
-selectUrgency.addEventListener("change", function () {
-  filters.urgency = selectUrgency.value;
-  applyFilters();
-});
-
-function applyFilters() {
-  visitCards.forEach(function (card) {
-    const cardStatus = card.querySelector(".visit-status").textContent.trim();
-    const cardUrgency = card.querySelector(".visit-urgency").textContent.trim();
-    const cardName = card
-      .querySelector("p")
-      .textContent.toLowerCase()
-      .replace(/\s/g, "");
-
-    const isNameMatch = cardName.includes(filters.searchText);
-
-    const isStatusMatch =
-      filters.status === defaultStatus ||
-      filters.status === "" ||
-      filters.status === cardStatus;
-
-    const isUrgencyMatch =
-      filters.urgency === defaultUrgency ||
-      filters.urgency === "" ||
-      filters.urgency === cardUrgency;
-
-    if (isNameMatch && isStatusMatch && isUrgencyMatch) {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
-    }
-  });
+      if (isNameMatch && isStatusMatch && isUrgencyMatch) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  }
 }
+
+const searchInput = document.getElementById('searchInput');
+const list = document.querySelector('.visit-wrap');
+const selectStatus = document.querySelector('.select-options');
+const selectUrgency = document.querySelector('.select-urgency');
+const visitCards = document.querySelectorAll('.visit-wrap .visit-card');
+
+const filters = new VisitFilters(list, visitCards);
+
+searchInput.addEventListener('input', () => {
+  filters.filters.searchText = searchInput.value.toLowerCase().replace(/\s/g, '');
+  filters.applyFilters();
+});
+
+selectStatus.addEventListener('change', () => {
+  filters.filters.status = selectStatus.value;
+  filters.applyFilters();
+});
+
+selectUrgency.addEventListener('change', () => {
+  filters.filters.urgency = selectUrgency.value;
+  filters.applyFilters();
+});
+
 
 const btn = document.querySelector(".authorization-btn");
 btn.addEventListener("click", (e) => {
