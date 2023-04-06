d91d7523bb039826ac17d70227d68fcf6d0bb3e2
Egoor_03 <e.zavertalyuk@gmail.com>
2023-04-06 21:40:39 +0300

    add removing function

--- js/visit.js
+++ js/visit.js
@@ -65,13 +65,21 @@ selectUrgency.addEventListener('change', () => {
   filters.applyFilters();
 });
 class VisitCard {
-  render({ doctor, name, priority }) {
+
+
+
+  render({ doctor, name, priority, id }) {
     const newCard = document.createElement('li');
     const randomVisit = ['Open', 'Done'];
     const randomIndex = Math.floor(Math.random() * randomVisit.length);
     const randomValue = randomVisit[randomIndex];
     newCard.classList.add('visit-card', 'border', 'rounded', 'border-primary-subtle');
+    newCard.id = id;
+    const divClose = document.createElement('div');
+    divClose.classList.add('closeModal');
+    newCard.prepend(divClose);
     newCard.innerHTML = `
+      <div class="closeModal"></div>
       <p class="patient-name text-center fs-5 mt-3"> ${name} </p>
       <p class="patient-doctor text-center fs-5"> ${doctor} </p>
       <div class="status-wrap hide">
@@ -85,6 +93,7 @@ class VisitCard {
     `;
 
     list.appendChild(newCard);
+    this.removeCard(newCard, newCard.id);
     newCard.addEventListener('click', (e) => {
       if (e.target.classList.contains('more-btn')) {
         const wrap = e.target.closest('.visit-card').querySelector('.status-wrap');
@@ -94,9 +103,21 @@ class VisitCard {
     })
     visitCards = document.querySelectorAll('.visit-wrap .visit-card');
     filters.applyFilters(visitCards);
-    return list;
+    return newCard;
+  }
+  removeCard(card, cardId) {
+    const closeModal = card.querySelector('.closeModal');
+    closeModal.addEventListener('click', async () => {
+      const delRequest = await request.deleteCard(localStorage.Authorization, cardId)
+      if (delRequest.ok) {
+        card.remove();
+      } else {
+        throw new Error('Ошибка запроса на сервер')
+      }
+    });
   }
 }
+
 
 
 export default class VisitForm {
