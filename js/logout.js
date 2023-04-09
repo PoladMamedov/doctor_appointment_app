a41b86ed09859fc271baa55cac8e002beb3e2dbe
Polad Mamedov <poladikksp@gmail.com>
2023-04-10 00:23:32 +0300

    added missing logOut btn, checkCards fix

--- js/logout.js
+++ js/logout.js
@@ -1,11 +1,11 @@
-
-export default  function logOut(e) {
-    e.preventDefault()
-    localStorage.removeItem("Authorization");
-    document.querySelector("#create-visit-btn").style.display = "none";
-    document.querySelector("#logout").style.display = "none";
-    const cards = [...document.querySelectorAll(".visit-card")];
-    cards.forEach((card) => card.remove())
-    document.querySelector(".authorization-btn").style.display = "block";
-
-  }
+import checkCards from "./checkCards.js";
+export default function logOut(e) {
+  e.preventDefault();
+  localStorage.removeItem("Authorization");
+  document.querySelector("#create-visit-btn").style.display = "none";
+  document.querySelector("#logout").style.display = "none";
+  const cards = [...document.querySelectorAll(".visit-card")];
+  cards.forEach((card) => card.remove());
+  document.querySelector(".authorization-btn").style.display = "block";
+  checkCards();
+}
