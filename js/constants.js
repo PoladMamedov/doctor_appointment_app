966d17945c3ab5ece6c57253abe59cf93ab4fca8
Egoor_03 <e.zavertalyuk@gmail.com>
2023-04-12 20:01:15 +0300

    del const

--- js/constants.js
+++
@@ -1,7 +0,0 @@
-const searchInput = document.getElementById('searchInput');
-const list = document.querySelector('.visit-wrap');
-const selectStatus = document.querySelector('.select-options');
-const selectUrgency = document.querySelector('.select-urgency');
-
-
-export { searchInput, list, selectStatus, selectUrgency}
