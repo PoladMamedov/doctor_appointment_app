98af4f6c67e24b9ac8bf86485bd33b2f763ebfab
Polad Mamedov <poladikksp@gmail.com>
2023-04-17 22:25:55 +0300

    fixed getAllCards

--- js/HideParameters.js
+++ js/HideParameters.js
@@ -1,13 +1,13 @@
 export default function toggleHideParameters(e) {
-   const wrap = e.target.closest('.visit-card').querySelector('.parameters-wrap');
-   const parametersWrap = e.target.closest('.visit-card').querySelector('.hide-parameters');
-   const hideAge = e.target.closest('.visit-card').querySelector('.hide-age');
-   hideAge.classList.toggle('hide')
-   parametersWrap.classList.toggle('hide');
-   wrap.classList.toggle('hide');
-   if (!wrap.classList.contains('hide')) {
-      e.target.textContent = 'Показать меньше'
-   } else {
-      e.target.textContent = 'Показать больше'
-   }
+  const wrap = e.target.closest(".visit-card").querySelector(".parameters-wrap");
+  const parametersWrap = e.target.closest(".visit-card").querySelector(".hide-parameters");
+  const hideAge = e.target.closest(".visit-card").querySelector(".hide-age");
+  hideAge.classList.toggle("hide");
+  parametersWrap.classList.toggle("hide");
+  wrap.classList.toggle("hide");
+  if (!wrap.classList.contains("hide")) {
+    e.target.textContent = "Показать меньше";
+  } else {
+    e.target.textContent = "Показать больше";
+  }
 }
